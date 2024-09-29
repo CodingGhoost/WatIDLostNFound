f53865459ec355b02d8b3df9462694841932af11
Wayne Hao <Wayne@Prowler.local>
2024-09-29 16:02:33 -0400

    Updated server.js

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,11 @@
 const express = require('express');
-const dotenv = require('dotenv');
-const cors = require('cors');
-const lostItemsRoutes = require('./routes/lostItemsRoutes');
-
-dotenv.config(); // Loads environment variables from .env file
 const app = express();
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5000;  // Use the Heroku-assigned port or default to 5000 for local development
 
-app.use(cors()); // Enable CORS
-app.use(express.json()); // Parse JSON request bodies
+// Middleware, routes, etc.
+app.use(express.json());
 
-// Root route
-app.get('/', (req, res) => {
-  res.send('Welcome to the Wat-ID Lost and Found API!');
-});
-
-// API routes
-app.use('/api/lost-items', lostItemsRoutes);
-
+// Start the server
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
